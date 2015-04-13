2e06db6524c6b7a3127a4a640c26415a8442f094
Cowtowncoder <tatu.saloranta@iki.fi>
2015-04-13 16:44:05 -0700

    Fix #3

--- src/test/java/com/fasterxml/jackson/datatype/jdk7/TestJdkTypes.java
+++ src/test/java/com/fasterxml/jackson/datatype/jdk7/TestJdkTypes.java
@@ -1,5 +1,6 @@
 package com.fasterxml.jackson.datatype.jdk7;
 
+import java.io.File;
 import java.nio.file.Path;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -10,8 +11,9 @@ public class TestJdkTypes extends ModuleTestBase
     {
         ObjectMapper mapper = mapperWithModule();
 
-        final String JSON = quote("/tmp/foo.txt");
-        
+        String sep = File.pathSeparator;
+        String JSON = quote(sep+"tmp"+sep+"foo.txt");
+
         Path p = mapper.readValue(JSON, Path.class);
         String output = mapper.writeValueAsString(p);
 
